d14d17d618327d144fc281f4b3b12159b8b0a387
minsun21 <mspark0429@daum.net>
2020-10-03 23:00:04 +0900

    init commit

---
+++ movieVideoApp/src/main/java/com/video/MovieVideoAppApplication.java
@@ -0,0 +1,13 @@
+package com.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVideoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieVideoAppApplication.class, args);
+	}
+
+}
